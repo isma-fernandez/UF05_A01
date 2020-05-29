7a1d65b73511d64c287db907ab497cabde0c329f
isma-fernandez <ismael-fernandez@outlook.es>
2020-05-29 17:42:29 +0200

    Primer commit

---
+++ src/base/UF5_A01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base;
+
+/**
+ *
+ * @author Ismael
+ */
+public class UF5_A01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
